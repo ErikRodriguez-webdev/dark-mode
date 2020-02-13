09b53125f38252e90e183839a23f9fda6f35a90b
Erik Rodriguez <eric19.21.03@gmail.com>
2020-02-12 19:45:06 -0800

    Boolean carried with function
    Add useDarkMode function that carries boolean in Navbar over to
    useDarkMode.js and over to useLocalStorage.js

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 
-export const useDarkMode = () => {
-  const [darkMode, setDarkMode] = useLocalStorage("darkMode");
+export const useDarkMode = (boolean) => {
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", boolean);
 
   useEffect(() => {
     const grabBody = document.querySelector("body");
